3f5c593bda8d8b6137d27c1d1f3f7b4437c9ed37
Ingo <ingo@star.ucl.ac.uk>
2015-02-05 18:16:23 +0000

    - disable cloud parameter file read. not sure why was enabled ... strange

--- classes/data.py
+++ classes/data.py
@@ -118,8 +118,8 @@ class data(base):
             self.rad = self.readfile(self.params.in_rad_file, interpolate=True)
         if params.in_include_cia:
             self.cia = self.readfile(self.params.in_cia_file, interpolate=True)
-        if params.in_include_cld:
-            self.cld = self.readfile(self.params.in_cld_file,interpolate=True)
+#         if params.in_include_cld:
+#             self.cld = self.readfile(self.params.in_cld_file,interpolate=True)
 
 
         #reading in Phoenix stellar model library (if emission is calculated only)
