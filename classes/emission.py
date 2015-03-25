- fixed some contribution function labelling and computation bugs

classes/emission.py
@@ -86,7 +86,7 @@ class emission(base):
         self.I_total    = np.zeros((self.nlambda),dtype=self.DTYPE)
         self.tau        = np.zeros((self.nlayers,self.nlambda),dtype=self.DTYPE)
         self.dtau       = np.zeros((self.nlambda,self.nlambda),dtype=self.DTYPE)
-        self.tau_total  = np.zeros((self.nlambda),dtype=self.DTYPE)
+        self.tau_total  = np.zeros((self.nlayers,self.nlambda),dtype=self.DTYPE)
 
         #loading c++ pathintegral library for faster computation
         if self.params.trans_cpp:
@@ -200,7 +200,8 @@ class emission(base):
             exptau =  np.exp(-1.0*self.tau[j,:]) 
             
             if temperature[j] != temperature[j-1]: 
-                BB_layer = em.black_body(self.specgrid,temperature[j])           
+                BB_layer = em.black_body(self.specgrid,temperature[j])   
+            self.tau_total[j,:] = BB_layer*(exptau) * (self.dtau[j,:])
             self.I_total += BB_layer*(exptau) * (self.dtau[j,:])
             
 

classes/output.py
@@ -655,7 +655,7 @@ class output(base):
 
                 out[:,1] = T_mean;
                 out[:,2] = T_sigma
-                filename = str(basename)+'/MCMC'+profilename+'_%i.dat' % (idx)
+                filename = str(basename)+'/MCMC'+profilename+'%i.dat' % (idx)
                 logging.info('Saving MCMC TP_profile to %s' % filename)
                 np.savetxt(filename,out)
 
@@ -682,7 +682,7 @@ class output(base):
             
                 out[:,1] = T_mean;
                 out[:,2] = T_sigma
-                filename = str(basename)+'/NEST'+profilename+'_%i.dat' % (idx)
+                filename = str(basename)+'/NEST'+profilename+'%i.dat' % (idx)
 
                 logging.info('Saving Nested Sampling TP profile for solution %i to %s' % (idx, filename))
                 np.savetxt(filename, out)              
@@ -703,7 +703,7 @@ class output(base):
         
         self.fitting.update_atmospheric_parameters(fit_params)
         tau, tau_total, dtau = self.fitting.forwardmodel.get_contribution_function()
-        np.savetxt(filename,tau)
+        np.savetxt(filename,tau_total)
 
     #
     #