6c1d9659ab7743f4db7db85f305dce2d9bcc71bb
Ingo Waldmann <ingo@star.ucl.ac.uk>
2015-05-10 10:32:27 +0100

    -small bug fixes all around

--- classes/cluster.py
+++ classes/cluster.py
@@ -115,7 +115,7 @@ class cluster(object):
 #             h.write('mpirun -np {0} -machinefile nodes  /share/apps/anaconda/2.2.0/bin/python taurex.py -p {1} -c {2} -i {3}'.format(TCPUS,PFILE,DICFILE,ID_number)+'\n') #run command
             h.write('mpirun -np {0} /share/apps/anaconda/2.2.0/bin/python taurex.py -p {1} -c {2} -i {3}'.format(TCPUS,PFILE,DICFILE,ID_number)+'\n') #run command
             h.write('mkdir -p '+OUTDIR+'\n')                       #make output dir
-            h.write('cp -rf '+SCRATCHDIR+'/* '+OUTDIR+'/'+'\n')    #copy all the stuff back
+            h.write('cp -rf '+SCRATCHDIR+'/Output/* '+OUTDIR+'/'+'\n')    #copy all the stuff back
             h.write('rm -rf '+SCRATCHDIR+' \n')                    #remove scratch dir
             
         return scriptname
